a9c0d2ad33a9bf3b2c4cc2fbc32e1a722e1ee17f
Stark <mrcs.stark@gmail.com>
2019-10-25 07:34:58 -0300

    Adiciona suite de testes

---
+++ src/test/java/SuiteTestes.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        GerenciadoresClientesTest.class,
+        GerenciadoresClientesTest_0.class,
+        GerenciadoresClientesTest_2.class,
+}) // Todas as classe que deseja ser executadas são adicionadas aqui
+public class SuiteTestes {
+
+
+}

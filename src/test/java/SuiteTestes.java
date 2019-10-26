113882295f83300adea6ca8a4943bed953588b24
Marcos Stark <marcosasouza.ti@gmail.com>
2019-10-26 13:20:58 -0300

    Atualizando Suite de testes

--- src/test/java/SuiteTestes.java
+++ src/test/java/SuiteTestes.java
@@ -3,9 +3,13 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+        GerenciadoraContasTest.class,
+        GerenciadoraContasTest_0.class,
+        GerenciadoraContasTest_1.class,
         GerenciadoresClientesTest.class,
         GerenciadoresClientesTest_0.class,
         GerenciadoresClientesTest_2.class,
+        GerenciadoresClientesTest_3.class,
 }) // Todas as classe que deseja ser executadas são adicionadas aqui
 public class SuiteTestes {
 
